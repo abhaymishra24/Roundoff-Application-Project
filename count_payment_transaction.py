bf2345e82198e8b58e2cfe868f5590b3def802a0
Abhay Mishra <techabhay06@gmail.com>
2024-10-09 10:31:50 +0530

    Add new commit

---
+++ count_payment_transaction.py
@@ -0,0 +1,33 @@
+
+
+
+# In this code we will count how many time payment done by each payment gateways.
+
+# Sample transaction data
+transactions = [
+    {"gateway": "Paytm"},
+    {"gateway": "GPay"},
+    {"gateway": "PhonePe"},
+    {"gateway": "Paytm"},
+    {"gateway": "GPay"},
+    {"gateway": "PhonePe"},
+    {"gateway": "Paytm"},
+]
+
+# Function to count payments by gateway
+def count_payments(transactions):
+    payment_count = {}
+    
+    for transaction in transactions:
+        gateway = transaction["gateway"]
+        if gateway in payment_count:
+            payment_count[gateway] += 1
+        else:
+            payment_count[gateway] = 1
+            
+    return payment_count
+
+# Count payments and print the result
+payment_summary = count_payments(transactions)
+print(payment_summary)
+    
