d7d9655019dee2228b8c87fef163d46b78740ad6
Abhay Mishra <techabhay06@gmail.com>
2025-01-17 22:03:10 +0530

    Add new commit

--- app1.py
+++ app1.py
@@ -1,33 +1,33 @@
 
 
 
-# In this code we will count how many time payment done by each payment gateways.
+# # In this code we will count how many time payment done by each payment gateways.
 
-# Find out transaction data from different payment system.
+# # Find out transaction data from different payment system.
 
-transactions = [
-    {"gateway": "Paytm"},
-    {"gateway": "GPay"},
-    {"gateway": "PhonePe"},
-    {"gateway": "Paytm"},
-    {"gateway": "GPay"},
-    {"gateway": "PhonePe"},
-    {"gateway": "Paytm"},
-]
+# transactions = [
+#     {"gateway": "Paytm"},
+#     {"gateway": "GPay"},
+#     {"gateway": "PhonePe"},
+#     {"gateway": "Paytm"},
+#     {"gateway": "GPay"},
+#     {"gateway": "PhonePe"},
+#     {"gateway": "Paytm"},
+# ]
 
-# Function to count payments by gateway
-def count_payments(transactions):
-    payment_count = {}
+# # Function to count payments by gateway
+# def count_payments(transactions):
+#     payment_count = {}
     
-    for transaction in transactions:
-        gateway = transaction["gateway"]
-        if gateway in payment_count:
-            payment_count[gateway] += 1
-        else:
-            payment_count[gateway] = 1
+#     for transaction in transactions:
+#         gateway = transaction["gateway"]
+#         if gateway in payment_count:
+#             payment_count[gateway] += 1
+#         else:
+#             payment_count[gateway] = 1
             
-    return payment_count
+#     return payment_count
 
-# Count payments and print the result
-payment_summary = count_payments(transactions)
-print(payment_summary)
+# # Count payments and print the result
+# payment_summary = count_payments(transactions)
+# print(payment_summary)
